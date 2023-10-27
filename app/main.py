0c1a1a137f52b9d2a3cc5048092ddf0ee9ac0208
Inna Isareva <inna8110302@gmail.com>
2023-10-27 13:00:40 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     #comment
-    return len([char for char in phrase if char.lower() == letter.lower()])
+    # return len([char for char in phrase if char.lower() == letter.lower()])
+    return f"ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg"
